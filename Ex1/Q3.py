02eb2aa4877b54e41233aced6f8e2173aaa62502
74805 <yuvtaub1308@gmail.com>
2024-02-07 19:49:47 +0200

    added square loss

--- Ex1/Q3.py
+++ Ex1/Q3.py
@@ -1,5 +1,15 @@
+import numpy as np
+
+
 def create_data_set():
     data = [(0, 0), (0, 1), (1, 0), (1, 1)]
     labels = [-1, 1, 1, -1]
     data_set = dict(zip(data, labels))
     return data_set
+
+
+def squared_loss(y_true, y_pred):
+    return np.mean((y_true - y_pred) ** 2)
+
+
+dataset = create_data_set()
